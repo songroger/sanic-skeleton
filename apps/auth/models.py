e7360b7e5260611bc1906aad0a43a6fa4eebf6ac
Fdk少东家 <978326643@qq.com>
2024-05-08 20:21:47 +0800

    删除多余无用的数据库模型

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -15,64 +15,6 @@ class FactoryParm(AbstractBaseModel):
         table = "factory_para_config"
 
 
-class FactoryRecord(AbstractBaseModel, TimestampMixin, UserMixin):
-    test_id = fields.UUIDField()
-    shelf_sn = fields.CharField(32)
-    test_case_category_one = fields.CharField(10)
-    test_case_category_two = fields.CharField(10)
-    test_case_result = fields.CharField(10)
-    test_case_comment = fields.TextField()
-    test_case_result_image_path = fields.JSONField()
-    test_case_ng_image_path = fields.JSONField()
-    is_deleted = fields.IntField()
-    test_case_ng_comment = fields.TextField()
-    test_case_name = fields.CharField(64)
-
-    def __str__(self):
-        return self.test_case_name
-
-    class Meta:
-        table = "factory_test_record"
-
-
-class FactoryTestSum(AbstractBaseModel, TimestampMixin, UserMixin):
-
-    test_id = fields.UUIDField()
-    test_strategy = fields.CharField(32)
-    shelf_sn = fields.CharField(20)
-    shelf_name = fields.CharField(32)
-    shelf_type = fields.CharField(32)
-    customer_name = fields.CharField(32)
-    order_id = fields.CharField(32)
-    test_case_result = fields.CharField(10, default="0")
-    is_deleted = fields.IntField(default=0)
-
-    def __str__(self):
-        return self.test_strategy
-
-    class Meta:
-        table = "factory_test_summary"
-
-
-class FactoryTestCase(AbstractBaseModel, TimestampMixin, UserMixin):
-
-    test_strategy = fields.CharField(32)
-    test_case_category_one = fields.CharField(10)
-    test_case_category_two = fields.CharField(10)
-    test_case_name = fields.CharField(64)
-    test_case_des = fields.TextField()
-    test_case_image_path = fields.JSONField()
-    is_disable = fields.IntField()
-    frontend_index = fields.CharField(32)
-    is_deleted = fields.IntField()
-
-    def __str__(self):
-        return self.test_case_name
-
-    class Meta:
-        table = "factory_test_case"
-
-
 class FactoryUser(AbstractBaseModel):
 
     create_time = fields.DatetimeField(null=True, auto_now_add=True)
